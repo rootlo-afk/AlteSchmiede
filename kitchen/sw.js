20276bff7633e0c6c20157f86ac1a6dd6723c015
rootlo-afk <rootlo@aol.com>
2026-04-11 23:12:48 +0200

    Update sw.js

--- kitchen/sw.js
+++ kitchen/sw.js
@@ -1,1 +1,7 @@
+self.addEventListener("install", (e) => {
+  console.log("Service Worker installiert");
+});
 
+self.addEventListener("fetch", (event) => {
+  // aktuell leer – später Cache möglich
+});
