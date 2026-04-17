68ac5900601441244568e036e45d58aec4b3223c
rootlo-afk <rootlo@aol.com>
2026-04-17 16:52:18 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker installiert");
-});
-
-self.addEventListener("fetch", (event) => {
-  // aktuell leer – später Cache möglich
-});
